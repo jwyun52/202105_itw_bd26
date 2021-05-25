a4dfd33de1078dea67fc9953a6824f78a8953788
JakeOh <jake.oh.lecture@gmail.com>
2021-05-25 17:04:15 +0900

    Add files via upload

--- lab_oracle/sql01.sql
+++ lab_oracle/sql01.sql
@@ -1,6 +1,10 @@
-USER이(가) "SCOTT"입니다.
+-- 첫번째 SQL 스크립트
 
-SYSDATE             
---------------------
-2021/05/25  16:57:48
+show user;  
+-- 현재 접속 사용자 계정 보여주기
 
+select sysdate from dual;  -- 현재시간 보기
+
+/*
+block comment(블록 주석)
+*/
