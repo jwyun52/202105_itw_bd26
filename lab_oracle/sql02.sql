f6d6ea58906c84dd85d7e42e1a5a4d70d0c2ce50
JakeOh <jake.oh.lecture@gmail.com>
2021-05-26 14:27:45 +0900

    Add files via upload

---
+++ lab_oracle/sql02.sql
@@ -0,0 +1,35 @@
+-- SQL 워크시트 생성(Alt+F10): 새 워크시트(접속) 생성
+
+-- CSV(Comma-Separated Values) 파일: 
+--  테이블의 값들이 comma로 구분된 형식의 텍스트 파일.
+--  comma로 값을 구분하는 경우가 일반적이지만, tab이나 colon(:) 등을 구분자로 사용하기도 함.
+
+-- 테이블 구조 확인
+-- desc 테이블이름;
+desc dept;
+desc emp;
+desc salgrade;
+
+-- SQL 명령어(키워드)는 대/소문자를 구분하지 않음.
+-- Oracle 데이터베이스 시스템은 테이블 이름과 컬럼 이름을 대문자로 변환 관리.
+
+-- Oracle의 데이터 타입(유형)
+--  (1) number: 숫자 타입(정수, 실수)
+--  (2) varchar2: variable-length characters. 가변길이 문자열.
+--  (3) date: 날짜/시간.
+
+-- 테이블에서 데이터를 검색
+--  (1) projection: 테이블에서 원하는 컬럼을 검색
+--  (2) selection: 테이블에서 조건을 만족하는 레코드(행)을 검색
+
+-- select 컬럼1, 컬럼2, ... from 테이블이름;
+-- 사원 테이블에서 사번, 사원 이름을 검색
+select empno, ename from emp;
+
+-- 사원 테이블에서 사번, 이름, 입사날짜, 급여를 검색
+select empno, ename, hiredate, sal from emp;
+select empno, ename, sal, hiredate from emp;
+
+-- 테이블에서 모든 컬럼(모든 자료)를 검색
+select * from emp;
+
