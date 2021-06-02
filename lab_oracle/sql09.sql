461acaa208892b5b13a51a867ea0c6721b3a6080
JakeOh <jake.oh.lecture@gmail.com>
2021-06-02 10:25:48 +0900

    Add files via upload

--- lab_oracle/sql09.sql
+++ lab_oracle/sql09.sql
@@ -11,6 +11,12 @@ from 테이블1 join종류 테이블2
     on join조건;
 */
 
+insert into emp (empno, ename, deptno)
+values (1004,  '오쌤', 50);
+commit;
+
+select * from emp;
+
 -- 사번, 이름, 부서번호, 부서이름 검색
 -- inner join
 select emp.empno, emp.ename, emp.deptno, dept.dname
@@ -31,6 +37,51 @@ where e.deptno = d.deptno;
 select e.empno, e.ename, e.deptno, d.dname
 from emp e inner join dept d
     on e.deptno = d.deptno;
+
+-- emp 테이블에서 부서번호 50번인 오쌤은 inner join 결과에 없음.
+-- 부서번호 50은 dept 테이블에 없기 때문에.
+-- dept 테이블에 있는 부서번호 40번 Operation도 inner join 결과에 없음.
+-- emp 테이블의 deptno 컬럼에는 40이 없기 때문에.
+
+
+-- left outer join
+-- 1) Oracle 문법
+select e.empno, e.ename, e.deptno, d.dname
+from  emp e, dept d
+where e.deptno = d.deptno(+);
+
+select e.empno, e.ename, d.deptno, d.dname
+from  emp e, dept d
+where e.deptno = d.deptno(+);
+
+-- 2) ANSI 표준 문법
+select e.empno, e.ename, e.deptno, d.dname
+from emp e left outer join dept d
+    on e.deptno = d.deptno;
+
+-- left outer join에서는
+-- 왼쪽 테이블 emp에만 등장하는 부서번호 50번에 대한 레코드(오쌤)도 함께 검색됨.
+-- 하지만, 오른쪽 테이블 dept에만 등장하는 부서번호 40번에 대한 
+-- 레코드(Operation)은 검색되지 않음.
+
+-- right outer join
+-- 1) Oracle 문법
+
+-- 2) ANSI 표준 문법
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
