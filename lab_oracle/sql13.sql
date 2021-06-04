00227297cccab65a3b1f3b7f0424a9c7e2434731
JakeOh <jake.oh.lecture@gmail.com>
2021-06-04 16:26:17 +0900

    Add files via upload

--- lab_oracle/sql13.sql
+++ lab_oracle/sql13.sql
@@ -39,5 +39,52 @@ create table ex_04 (
     ex_id   number(2),
     ex_name varchar2(10 char),
     -- 제약 조건 정의
-    
+    constraint pk_ex04_id primary key (ex_id),
+    constraint nn_ex04_name check (ex_name is not null)
 );
+
+-- 테이블 이름: ex_05
+-- 컬럼:
+--   (1) id: 숫자 2자리, PK
+--   (2) name: 문자열 10글자, NN
+--   (3) code: 문자열 3 byte, 중복되지 않는 값
+--   (4) create_date: 날짜, 기본값 현재시간
+--   (5) age: 숫자 3자리, 0보다 크거나 같아야 함.
+--   (6) gender: 문자열 1 byte, 'M' 또는 'F'만 가능
+-- 모든 제약 조건을 inline으로
+create table ex_05 (
+    id          number(2)
+                constraint pk_ex05_id primary key,
+    name        varchar2(10 char)
+                constraint nn_ex05_name not null,
+    code        varchar2(3 byte)
+                constraint uq_ex05_code unique,
+    create_date date default sysdate,
+    age         number(3)
+                constraint ck_ex05_age check (age >= 0),
+    gender      varchar2(1 byte)
+                constraint ck_ex05_gender check (gender in ('M', 'F'))
+                -- check (gender = 'M' or gener = 'F')
+);
+
+-- 테이블 이름: ex_06
+-- ex_05 테이블과 구조/제약조건이 같은 테이블을 생성.
+-- 제약조건 정의와 컬럼 정의를 구분
+create table ex_06 (
+    id          number(2),
+    name        varchar2(10 char),
+    code        varchar2(3 byte),
+    create_date date default sysdate,
+    age         number(3),
+    gender      varchar2(1 byte),
+    -- constraints
+    constraint pk_ex06_id primary key (id),
+    constraint nn_ex06_name check (name is not null),
+    constraint uq_ex06_code unique (code),
+    constraint ck_ex06_age check (age >= 0),
+    constraint ck_ex06_gender check (gender in ('M', 'F'))
+);
+
+
+
+
