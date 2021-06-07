3e73160edb4a7a2f072fdd4b7e422ce3abe7b0d5
JakeOh <jake.oh.lecture@gmail.com>
2021-06-07 11:31:53 +0900

    Add files via upload

--- lab_oracle/sql14.sql
+++ lab_oracle/sql14.sql
@@ -9,6 +9,41 @@
   4) cust_gender: 고객 성별. 1자리 정수. 기본값 0. (0, 1, 2) 중 1개 값만 가능.
   5) cust_age: 고객 나이. 3자리 정수. 기본값 0. 0 이상 200 이하의 정수만 가능.
 */
+create table customers (
+    cust_id     varchar2(20)
+                constraint pk_customers primary key
+                constraint ck_cust_id check (lengthb(cust_id) >= 8),
+    cust_pw     varchar2(20)
+                constraint nn_cust_pw not null
+                constraint ck_cust_pw check (lengthb(cust_pw) >= 8),
+    cust_email  varchar2(100),
+    cust_gender number(1) default 0
+                constraint ck_cust_gender check (cust_gender in (0, 1, 2)),
+                -- constraint ck_cust_gender check (cust_gender = 0 or cust_gender = 1 or cust_gender = 2)
+    cust_age    number(3) default 0
+                constraint ck_cust_age check (cust_age between 0 and  200)
+                -- constraint ck_cust_age check (cust_age >= 0 and  cust_age <= 200)
+);
+
+create table customers2 (
+    cust_id     varchar2(20),           
+    cust_pw     varchar2(20),
+    cust_email  varchar2(100),
+    cust_gender number(1) default 0,
+    cust_age    number(3) default 0,
+    
+    constraint pk_cust2 primary key (cust_id),
+    constraint ck_id2 check (lengthb(cust_id) >= 8),
+    constraint ck_pw2 check (cust_pw is not null and lengthb(cust_pw) >= 8),
+    constraint ck_gender2 check (cust_gender in (0, 1, 2)),
+    constraint ck_age2 check (cust_age between 0 and 200)
+);
+
+-- Oracle이 테이블들을 관리하기 위해 가지고 있는 테이블
+desc user_tables;
+-- Oracle이 제약조건들을 관리하기 위해 가지고 있는 테이블
+desc user_constraints;
+select constraint_name, table_name from user_constraints;
 
 
 /*
