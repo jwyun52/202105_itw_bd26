3c3834e53aa56c302e42f159e34f877d72e68368
JakeOh <jake.oh.lecture@gmail.com>
2021-06-10 12:19:40 +0900

    Add files via upload

--- lab_r/r05_dataframe.R
+++ lab_r/r05_dataframe.R
@@ -21,3 +21,17 @@ str(exam)
 #   dbl: double. 실수
 #   chr: character. 문자(열)
 #   logi: logical. 논릿값
+
+# summary(df): 데이터 프레임의 기술 통계량(descriptive statistics) 요약.
+summary(exam)
+
+# summary(df$var): 특정 컬럼의 기술 통계량.
+summary(exam$math)
+
+# summary() 함수가 보여주는 기술 통계량
+# Min.: minimum, 최솟값
+# Max.: maximum, 최댓값
+# Mean: 평균
+# Median: 중앙값
+# 1st Qu.: first Quartile, 1사분위값
+# 3rd Qu.: third Quratile, 3사분위값
