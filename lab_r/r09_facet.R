a4520cf91a5d88a3f67473e0cd63477cfd8d8358
JakeOh <jake.oh.lecture@gmail.com>
2021-06-14 10:34:57 +0900

    Add files via upload

---
+++ lab_r/r09_facet.R
@@ -0,0 +1,36 @@
+# Faceting: 
+# 한 개의 plot을 카테고리별로 행과 열을 나눠서 여러개의 그래프를 그리는 것.
+
+search()
+library(tidyverse)
+
+head(mpg)
+
+# drv별 cty ~ displ
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = cty, color = drv))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = cty)) +
+  facet_wrap(facets = ~ drv)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = cty)) +
+  facet_grid(facets = . ~ drv)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = cty)) +
+  facet_grid(facets = drv ~ .)
+
+# facet_wrap(): 행 또는 열 한 방향으로만 칸을 나눌 수 있음.
+# facet_grid(): 행, 열 방향 모두 칸을 나눌 수 있음.
+
+# drv별, cyl별 hwy ~ displ 산점도 그래프
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy,
+                           color = drv, shape = as.factor(cyl)))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(facets = cyl ~ drv)
+
