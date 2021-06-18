d08bced6dc9e51bf3c3cfbd1e6944bf40f79d0a6
JakeOh <jake.oh.lecture@gmail.com>
2021-06-18 16:24:54 +0900

    Add files via upload

--- lab_r/r18_koweps.R
+++ lab_r/r18_koweps.R
@@ -47,3 +47,39 @@ ggplot(data = welfare) +
 populations %>% 
   pivot_wider(names_from = gender, values_from = n) %>% 
   mutate(Female_Ratio = Female / (Male + Female))
+
+# 지역별, 연령대별 인구수 -> pivot
+welfare %>% 
+  count(region_code, age_range) %>% 
+  pivot_wider(names_from = age_range, values_from = n)
+
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(y = region_code, fill = age_range),
+           position = 'fill')
+
+# 지역별, 연령대별, 성별 인구수 -> 지역별 통계가 행(row)가 되도록 pivot
+welfare %>% 
+  count(region_code, age_range, gender) %>% 
+  pivot_wider(names_from = c(age_range, gender), values_from = n)
+  # pivot_wider(names_from = gender, values_from = n)
+
+
+# welfare 데이터 프레임에 ages 파생변수를 추가
+#   ages = 'young', if age < 30
+#   ages = 'middle', if age < 60
+#   ages = 'old', if age >= 60
+# 파생변수 ages를 factor로 변환
+welfare <- welfare %>% 
+  mutate(ages = ifelse(age < 30, 'young',
+                       ifelse(age < 60, 'middle', 'old')))
+welfare
+
+welfare$ages <- factor(welfare$ages,
+                       levels = c('young', 'middle', 'old'))
+# level과 동일한 label을 사용하는 경우 labels 파라미터는 생략.
+
+welfare
+table(welfare$ages)
+
+# 지역별, ages별 인구수 테이블
+# 지역별, ages의 비율 막대 그래프
