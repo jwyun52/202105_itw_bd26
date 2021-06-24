a6b082288896dcad758dad29d592f4a14bd7275c
JakeOh <jake.oh.lecture@gmail.com>
2021-06-24 18:20:20 +0900

    Add files via upload

--- lab_r/rml01_knn.R
+++ lab_r/rml01_knn.R
@@ -11,14 +11,16 @@ library(class)
 library(gmodels)
 search()
 
-# 데이터 셋 준비
+
+# 1. 데이터 셋 준비 -----
 ?datasets::iris  # help(도움말) 페이지
 iris <- datasets::iris  # datasets 패키지의 iris 데이터를 메모리에 복사.
 head(iris)
 tail(iris)
 str(iris)
 
-# 데이터 탐색(exploration)
+
+# 2. 데이터 탐색(exploration) -----
 # 탐색적 데이터 분석(EDA: exploratory data analysis)
 summary(iris)
 #> 수치형 데이터 - 기술 통계량, factor 타입 데이터 - 빈도수
@@ -39,15 +41,18 @@ ggplot(data = iris) +
 ggplot(data = iris) +
   geom_boxplot(mapping = aes(x = Species, y = Petal.Width))
 
+# 변수 간의 상관 관계
 # Petal.Length ~ Petal.Width 산점도 그래프.
 # 점의 색깔을 붓꽃의 품종에 따라서 다르게 표현.
 ggplot(data = iris) +
   geom_point(mapping = aes(x = Petal.Width, y = Petal.Length,
                            color = Species))
 
-# kNN 알고리즘 적용 예측
+
+# 3. kNN 알고리즘 적용 예측 -----
 # 1) iris 데이터 프레임을 특성(features)과 타겟(target)으로 분리.
 # 2) 전체 데이터 셋을 훈련 셋과 테스트 셋으로 나눔.
+
 # 인덱스를 사용해서 데이터 프레임에서 원소를 선택하는 방법
 # data_frame[row_index, column_index]
 iris[1, 1]  #> 1번째 행, 1번째 열의 원소
@@ -60,3 +65,48 @@ iris[c(1, 3, 5), ]  #> 1, 3, 5번 행의 모든 열의 원소
 iris[1]  #> 1번째 열의 모든 원소
 iris[1:4]  #> 1~4번째 열의 모든 원소
 iris[c(1, 3, 5)]  #> 1, 3, 5번째 열의 모든 원소
+
+# 전체 데이터 셋을 단순히 순서대로 1~100행, 101~150행로 나누면
+# virginica 품종은 훈련이 되지 않는 문제가 발생. ==> 훈련 데이터 편향
+iris[1:50, ]  #> setosa
+iris[51:100, ]  #> versicolor
+iris[101:150, ]  #> virginica
+
+# 층화 (임의) 추출(stratified sampling):
+# 전체 데이터에서 훈련 셋이 편향되지 않도록, 
+# 각 클래스에서 고르게 샘플들을 추출해서 훈련 셋을 만드는 것.
+
+# 임의 추출(random sampling):
+# 전체 데이터를 무작위로 섞어서 훈련 셋을 만드는 것.
+
+idx <- sample(150)
+# sample(n): 1 ~ n까지 정수를 무작위로 섞어서 반환.
+idx  # 1~150 정수들이 무작위로 섞인 상태
+train_row <- idx[1:120]  # 훈련 셋으로 사용할 행 번호
+test_row <- idx[121:150] # 테스트 셋으로 사용할 행 번호
+train <- iris[train_row, ]  # 훈련 셋
+test <- iris[test_row, ]  # 테스트 셋
+
+table(train$Species)
+table(test$Species)
+
+# 훈련 셋/테스트 셋의 특성과 타겟을 분리
+train_features <- train[, 1:4]  # 훈련 셋 특성들(features)
+train_target <- train[, 5]  # 훈련 셋 타겟(target)
+head(train_features)
+head(train_target)
+
+test_features <- test[, 1:4]  # 테스트 셋 특성들
+test_target <- test[, 5]  # 테스트 셋 타겟
+
+# kNN 알고리즘에 훈련 셋 특성/타겟, 테스트 셋 특성을 적용해서
+# 테스트 셋의 타겟을 예측
+test_predictions <- knn(train = train_features,
+                        cl = train_target,
+                        test = test_features,
+                        k = 3)
+test_predictions  # 테스트 셋의 예측값
+
+# 테스트 셋의 예측값과 실제값을 비교
+test_predictions == test_target
+mean(test_predictions == test_target)
