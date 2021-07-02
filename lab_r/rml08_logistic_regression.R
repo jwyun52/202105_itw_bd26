cbaa0a9a1122fdfc6ccbb608e9596ce8278b90f8
JakeOh <jake.oh.lecture@gmail.com>
2021-07-02 14:39:58 +0900

    Add files via upload

--- lab_r/rml08_logistic_regression.R
+++ lab_r/rml08_logistic_regression.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(gmodels)  # CrossTable() 함수
 
 # Logistic Regression(로지스틱 회귀)를 사용한 분류
 x <- seq(-5, 5, 0.001)
@@ -36,5 +37,35 @@ test_set <- wisc_bc_data[(tr_size + 1):n, 2:32]
 # logistic regression 모델 훈련
 log_reg <- glm(formula = diagnosis ~ .,
                data = train_set,
-               family = 'binomial')  # 2항 분류(binomial classification)
+               family = 'binomial',  # 2항 분류(binomial classification)
+               maxit = 100)  # 알고리즘이 수렴하지 않는 경고 없애기 위해서
 log_reg
+
+# 훈련 셋 예측 확률(diagnosis이 1(M)이 될 확률)
+train_probabilities <- predict(log_reg, train_set, 
+                               type = 'response')
+head(train_probabilities)
+
+# 훈련 셋 예측 값
+train_predictions <- ifelse(train_probabilities > 0.5, 1, 0)
+head(train_predictions)
+table(train_predictions)
+
+# 훈련 셋 정확도
+mean(train_set$diagnosis == train_predictions)  #> 100%
+
+# 테스트 셋 예측 확률
+test_probabilities <- predict(log_reg, test_set,
+                              type = 'response')
+head(test_probabilities)
+
+# 테스트 셋 예측값
+test_predictions <- ifelse(test_probabilities > 0.5, 1, 0)
+head(test_predictions)
+
+# 테스트 셋 정확도
+mean(test_set$diagnosis == test_predictions)  #> 96.5%
+
+# confusion matrix(혼동 행렬, 오차 행렬)
+CrossTable(test_set$diagnosis, test_predictions, 
+           prop.chisq = FALSE)
